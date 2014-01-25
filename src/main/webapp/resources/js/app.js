bea9afb986ee92cd7054f39a9d7a6a366d1fcd88
Shashidhar Dakuri <shashidhar.dakuri@gmail.com>
2014-01-24 19:21:22 -0800

    Player registration complete

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -2,22 +2,23 @@
 
 var FoosBarApp = {};
 
-var App = angular.module('FoosBarApp',[]);
+var App = angular.module('FoosBar',[]);
 
-App.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('#register', {
-            controller: PlayerController
-        });
-    $routeProvider.otherwise('/', {
-            templateUrl: 'index'
-        });
-}]);
+
 
 /**
  * PlayerController
  * @Constructor
  */
 var PlayerController = function($scope, $http){
+    $scope.registerPlayer = function(player){
+        $http.post('player/register', player).success(function() {
+            console.log("Yay, created player")
+        });
+    }
+}
+
+function registration($scope, $http){
     $scope.registerPlayer = function(player){
         $http.post('player/register', player).success(function() {
             console.log("Yay, created player")
